5572e52ea8151552a8739b2742a834ee504d6078
Joe Jordan <lankyjoe@gmail.com>
2021-05-28 15:57:15 -0700

    Node functions & documentation
    Added details about how API can be used in Documentation file; added placeholder functionality for node requests

--- index.js
+++ index.js
@@ -72,6 +72,8 @@ let films = [
   }
 ];
 
+let users = [];
+
 // GET requests
 
 app.get('/', (req, res) => {
@@ -84,7 +86,7 @@ app.get('/documentation', (req, res) => {
 
 app.use(express.static('public'));
 
-// Gets the list of data about ALL docs
+// Gets the list of data about ALL films
 
 app.get('/films', (req, res) => {
   res.json(films);
@@ -97,7 +99,83 @@ app.get('/films/:title', (req, res) => {
     { return film.title === req.params.title }));
 });
 
+// Gets the data about a genre, by name
 
+app.get('/genres/:name', (req, res) => {
+  res.json(genres.find((genre) =>
+    { return genre.name === req.params.name }));
+});
+
+// Gets the data about a director, by name
+
+app.get('/directors/:name', (req, res) => {
+  res.json(directors.find((director) =>
+    { return director.name === req.params.name }));
+});
+
+// Allows a new user to register
+
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing username in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Updates a user's profile
+
+app.put('/users/:username/profile', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+
+  if (user) {
+    user.profile[req.params.username] = parseInt(req.params.username);
+    res.status(201).send('Profile for ' + req.params.username + ' has been updated.');
+  } else {
+    res.status(404).send('User with the name ' + req.params.username + ' was not found.');
+  }
+});
+
+// Adds a film to a user's list of favorites
+
+app.post('/users/:username/favorites', (req, res) => {
+  let newFave = req.body;
+
+  if (!newFave.title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    users.favorites.push(newFave);
+    res.status(201).send(newFave);
+  }
+});
+
+// Removes a film from a user's list of favorites
+
+app.delete('/users/:username/favorites/:title', (req, res) => {
+  let film = films.find((film) => { return film.title === req.params.title });
+
+  if (film) {
+    films = films.filter((obj) => { return obj.title !== req.params.title });
+    res.status(201).send(req.params.title + ' was removed from favorites.');
+  }
+});
+
+// De-registers a user
+
+app.delete('/users/:username', (req, res) => {
+  let user = users.find((user) => { return user.username === req.params.username });
+
+  if (user) {
+    user = users.filter((obj) => { return obj.username !== req.params.username });
+    res.status(201).send('User ' + req.params.username + ' has been de-registered.');
+  }
+});
 
 // log errors
 app.use((err, req, res, next) => {
