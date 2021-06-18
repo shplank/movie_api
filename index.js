0c2095ff8236dc5b172e1fa63b901f09e0d2b64c
Joe Jordan <lankyjoe@gmail.com>
2021-06-18 15:23:10 -0700

    Update index.js
    Updated URI connection

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/moooviesdb', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/moooviesdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   morgan = require('morgan'),
