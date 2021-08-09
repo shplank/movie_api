7de834bc83a7c64c2064a46fd1a48bd771f8d9a3
Joe Jordan <lankyjoe@gmail.com>
2021-08-09 15:51:14 -0700

    Update index.js
    eliminated "favorites" endpoint

--- index.js
+++ index.js
@@ -137,18 +137,6 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
 // Get a user by username
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOne({ Username: req.params.Username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-// Get a user by username
-app.get('/favorites/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOne({ Username: req.params.Username })
   .populate({path: 'Favorites', model: Films})
     .then((user) => {
       res.json(user);
